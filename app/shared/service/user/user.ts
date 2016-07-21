3a25ec3b91643a57d438b4f7d4ab7aca4e8a48d3
eduray <egcabc@gmail.com>
2016-07-20 19:41:43 -0600

    Se agrega interfaz User

---
+++ app/shared/service/user/user.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by eduray on 7/16/16.
+ */
+export interface User {
+    userName:string;
+    name:string;
+    email:string;
+    firstName:string;
+    lastName:string;
+    pseudonym:string;
+    password:string;
+    countryId:number;
+    accountId:number;
+    location:string;
+}
