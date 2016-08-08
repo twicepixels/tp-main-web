1d9ecb78bc31ee21a0c92325dd6e6d27a729d127
dfonseca <tuma05@gmail.com>
2016-08-08 13:58:19 -0600

    Eliminar servicios no requeridos en el main

--- app/main.ts
+++ app/main.ts
@@ -1,26 +1,24 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 // Providers
-import {enableProdMode} from '@angular/core';
+import { enableProdMode } from '@angular/core';
 
 import { HTTP_PROVIDERS } from '@angular/http';
 import { APP_ROUTER_PROVIDERS } from "./app.routes";
 // Components
-import { AuthService } from './shared/service/auth/auth.service';
 import { RestService } from './shared/service/rest.service';
+import { AuthService } from './shared/service/auth/auth.service';
 import { MainComponent } from './shared/template/page/main.component';
-import { disableDeprecatedForms, provideForms } from '@angular/forms';
-import {CustomerAccountService} from "./services/customer/account/customer.account.service";
 
 enableProdMode();
 bootstrap(MainComponent, [
-		AuthService,
-	    RestService,
-	    CustomerAccountService,
 		HTTP_PROVIDERS,
 		APP_ROUTER_PROVIDERS,
-	    disableDeprecatedForms(),
-	    provideForms(),
+		disableDeprecatedForms(),
+		provideForms(),
+		AuthService,
+		RestService,
 		{provide: LocationStrategy, useClass: HashLocationStrategy}
 	]
 ).catch(err => console.error(err));
