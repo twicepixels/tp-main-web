acaf77546d498ab5652f06fc96c7eef55fc46747
dfonseca <tuma05@gmail.com>
2016-08-19 21:10:33 -0600

    Eliminar rutas del navbar y colocarlas en el archivo base

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,9 +1,21 @@
 import { provideRouter, RouterConfig }       from '@angular/router';
+import { HomeComponent } from "./components/home.component";
+import { LoginComponent } from "./shared/template/login/login.component";
+import { FormCustomerUserComponent } from "./components/customer/user/customer.user.component";
+import { FormCustomerAccountComponent } from "./components/customer/account/customer.account.component";
+import { FormChangePasswordComponent } from "./components/customer/user/password.change.component";
 // Routes
-import { NavbarRoutes } from './shared/template/navbar/navbar.routes'
+const SystemRoutes: RouterConfig = [
+	{path: '', component: HomeComponent},
+	{path: 'home', component: HomeComponent},
+	{path: 'login', component: LoginComponent},
+	{path: 'user', component: FormCustomerUserComponent},
+	{path: 'account', component: FormCustomerAccountComponent},
+	{path: 'changePassword', component: FormChangePasswordComponent}
+];
 
 export const routes: RouterConfig = [
-	...NavbarRoutes
+	...SystemRoutes
 ];
 export const APP_ROUTER_PROVIDERS = [
 	provideRouter(routes)
