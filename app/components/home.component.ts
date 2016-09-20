f04739dca0f13a4eb0d0b0c91d578b5a854bf442
dfonseca <tuma05@gmail.com>
2016-09-20 14:52:11 -0600

    Versión 1 del home page

--- app/components/home.component.ts
+++ app/components/home.component.ts
@@ -6,6 +6,7 @@ import { SearchComponent } from "../shared/template/search/search.component";
 @Component({
 	selector: 'home-section',
 	template: require('./home.component.html'),
+	styles: [require('./home.parallax.less')],
 	directives: [SearchComponent]
 })
 export class HomeComponent extends BaseComponent {
